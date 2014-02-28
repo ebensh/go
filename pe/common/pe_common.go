e031bd39d1c3bb5f9b4d02fcfbdebf71d7989de1
Evan Benshetler <ebensh@google.com>
2014-02-28 18:24:07 -0500

    Fixing dir structure

---
+++ pe/common/pe_common.go
@@ -0,0 +1,29 @@
+// Package pecommon contains common ProjectEuler utility classes and methods.
+package pecommon
+
+import (
+	"sort"
+	"strconv"
+)
+
+// ByteSlice attaches the methods of Interface to []byte,
+// sorting in increasing order.
+type ByteSlice []byte
+
+func (p ByteSlice) Len() int { return len(p) }
+
+// We reverse Less here to mean More so that 0's go at the right end,
+// instead of getting lost in the hash.
+func (p ByteSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p ByteSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByteSlice) Sort()              { sort.Sort(p) }
+
+// This may not
+func ToDigits(i int) []int {
+	digits_bytes := ByteSlice(strconv.FormatInt(int64(i), 10))
+	digits_ints := make([]int, digits_bytes.Len())
+	for index, digit_byte := range digits_bytes {
+		digits_ints[index] = int(digit_byte - '0')
+	}
+	return digits_ints
+}
